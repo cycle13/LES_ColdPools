71c0d2380dc5945f10e33265a40f75ebf25a5c8a
meyerbe <meyerbe@ethz.ch>
2018-10-09 21:48:28 +0200

    rename

--- define_cp_rim_v1.py
+++ define_cp_rim_v1.py
@@ -7,16 +7,13 @@ import argparse
 import json as simplejson
 import os
 
-
-plt.rcParams['lines.linewidth'] = 1.
-
 # (a) vertical velocity is zero from divergence to convergence zone (behind front)
 #       >> velocity should be zero when averaged over a larger area
 # (b) radially average
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='PyCLES')
+    parser = argparse.ArgumentParser(prog='LES_CP')
     parser.add_argument("--casename")
     parser.add_argument("--path")
     parser.add_argument("--tmin")
@@ -28,9 +25,9 @@ def main():
     if args.path:
         path = args.path
     else:
-        # path = '/Users/bettinameyer/polybox/ClimatePhysics/Copenhagen/Projects/LES_ColdPool/' \
-        #        'triple_3D_noise/Out_CPDry_triple_dTh2K/'
-        path = '/nbi/ac/cond1/meyerbe/ColdPools/triple_3D_noise/Out_CPDry_triple_Th3K/'
+        path = '/Users/bettinameyer/polybox/ClimatePhysics/Copenhagen/Projects/LES_ColdPool/' \
+                'triple_3D_noise/Out_CPDry_triple_dTh2K/'
+        # path = '/nbi/ac/cond1/meyerbe/ColdPools/triple_3D_noise/Out_CPDry_triple_Th3K/'
     if os.path.exists(os.path.join(path, 'fields')):
         path_fields = os.path.join(path, 'fields')
     elif os.path.exists(os.path.join(path, 'fields_k120')):
@@ -68,12 +65,13 @@ def main():
 
     global cm_bwr, cm_grey, cm_vir
     cm_bwr = plt.cm.get_cmap('bwr')
-    cm_vir = plt.cm.get_cmap('jet')
+    cm_vir = plt.cm.get_cmap('viridis')
     cm_grey = plt.cm.get_cmap('gist_gray_r')
 
     # define subdomain to scan
     # --- for triple coldpool ---
-    d = np.int(np.round(ny / 2))
+    # d = np.int(np.round(ny / 2))      # nbi
+    d = np.int(np.round(ny / 2)) + gw
     a = np.int(np.round(d * np.sin(60.0 / 360.0 * 2 * np.pi)))  # sin(60 degree) = np.sqrt(3)/2
     rstar = 5000.0  # half of the width of initial cold-pools [m]
     irstar = np.int(np.round(rstar / dx))
@@ -82,8 +80,8 @@ def main():
     shift = 20
     id = irstar + shift
     jd = irstar + shift
-    ishift = np.max(id - ic, 0)
-    jshift = np.max(jd - jc, 0)
+    ishift = np.max(id-ic,0)
+    jshift = np.max(jd-jc,0)
     nx_ = 2 * id
     ny_ = 2 * jd
 
@@ -98,15 +96,15 @@ def main():
     if args.k0:
         k0 = np.int(args.k0)
     else:
-        k0 = 5          # level
-    dphi = 6            # angular resolution for averaging of radius
+        k0 = 5      # level
+    dphi = 6        # angular resolution for averaging of radius
     n_phi = 360 / dphi
     # - rim_intp_all = (phi(t,i_phi)[deg], phi(t,i_phi)[rad], r(t,i_phi))
     # - rim_vel = (phi(t,i_phi)[deg], phi(t,i_phi)[rad], r(t,i_phi), U(t,i_phi), dU(t, i_phi))
-    # - rim_vel_av 0 (r_av(t), U_av(t), dU_av/dt(t))
-    rim_intp_all = np.ndarray(shape=(3, nt, n_phi), dtype=np.double)
-    rim_vel = np.ndarray(shape=(5, nt, n_phi), dtype=np.double)
-    rim_vel_av = np.ndarray(shape=(3, nt))
+    # - rim_vel_av = (r_av(t), U_av(t), dU_av/dt(t))
+    rim_intp_all = np.zeros(shape=(3, nt, n_phi), dtype=np.double)
+    rim_vel = np.zeros(shape=(5, nt, n_phi), dtype=np.double)
+    rim_vel_av = np.zeros(shape=(3, nt))
 
     for it,t0 in enumerate(timerange):
         if it > 0:
@@ -117,11 +115,11 @@ def main():
 
         '''(A) read in w-field, shift domain and define partial domain '''
         w = read_in_netcdf_fields('w', os.path.join(path_fields, str(t0) + '.nc'))
-        w_roll = np.roll(np.roll(w[:, :, k0], ishift, axis=0), jshift, axis=1)
+        # w_roll = np.roll(np.roll(w[:, :, k0], ishift, axis=0), jshift, axis=1)    # nbi
+        w_roll = np.roll(w[:, :, k0], [ishift, jshift], [0, 1])
         w_ = w_roll[ic - id + ishift:ic + id + ishift, jc - jd + jshift:jc + jd + jshift]
         icshift = id
         jcshift = jd
-        print('ishift', ishift, ic-id + ishift, ic+ id + ishift, icshift, jcshift)
 
         plot_yz_crosssection(w, ic, path_out, t0)
 
@@ -223,7 +221,7 @@ def main():
                         if a <= 5 or a >= 9:
                             break
 
-        plot_outlines(perc, w_mask_r, rim, rim2, rim_test, rim_test1, rim_test2,
+        plot_outlines(perc, w_mask, rim, rim2, rim_test, rim_test1, rim_test2,
                       jcshift, nx_, ny_, t0)
 
         del rim_test, rim_test1, rim_test2
@@ -238,16 +236,20 @@ def main():
         nrim = len(rim_list)
         for i, coord in enumerate(rim_list):
             rim_list[i] = (coord, (polar(coord[0]-icshift, coord[1]-jcshift)))
-            # if rim already very close to subdomain (nx_,ny_), make domain larger
-            if coord[0] >= nx_ - 3 or coord[1] >= ny_ - 3:
-                print('!!! changing domain size', nx_, nx_+4)
-                shift += 5
-                id = irstar + shift
-                jd = irstar + shift
-                ishift = np.max(id - ic, 0)
-                jshift = np.max(jd - jc, 0)
-                nx_ = 2 * id
-                ny_ = 2 * jd
+        # test polar coordinate transformation
+        test_polar_fct()
+
+        # if rim already very close to subdomain (nx_,ny_), make domain larger
+        if coord[0] >= nx_ - 3 or coord[1] >= ny_ - 3:
+            print('!!! changing domain size', nx_, nx_+4)
+            shift += 5
+            id = irstar + shift
+            jd = irstar + shift
+            ishift = np.max(id - ic, 0)
+            jshift = np.max(jd - jc, 0)
+            nx_ = 2 * id
+            ny_ = 2 * jd
+
         # sort list according to angle
         rim_list.sort(key=lambda tup: tup[1][1])
         plot_rim_mask(w_, rim, rim_list, rim_list_backup, icshift, jcshift, nx_, ny_, t0)
@@ -256,11 +258,14 @@ def main():
 
 
         # average and interpolate for bins of 6 degrees
+        # rim_list_int = []
         # rim_intp = (phi[deg], phi[rad], r(phi))
         # rim_intp_all = (phi[t,deg], phi[t,rad], r(t,phi))
+        rim_intp = np.ndarray(shape=(3,n_phi), dtype=np.double)
         angular_range = np.arange(0,361,dphi)
-        rim_intp_all[0,it,:] = angular_range[:-1]
-        rim_intp_all[1,it,:] = np.pi*rim_intp_all[0,it,:]/180
+        rim_intp[0,:] = angular_range[:-1]
+        rim_intp[1,:] = np.pi*rim_intp[0,:]/180
+        rim_intp_all[0:2,it,:] = rim_intp[0:2,:]
         # print('')
         i = 0
         for n,phi in enumerate(rim_intp_all[0,it,:]):
@@ -279,18 +284,22 @@ def main():
                     # >> could probably be done more efficiently
                     break
             if count > 0:
-                r_aux /= count
-            rim_intp_all[2,it,n] = r_aux
+                rim_intp[2,n] = r_aux / count
+                rim_intp_all[2,it,n] = r_aux / count
+            # rim_list.append((phi,r_aux))
         print('')
 
 
         # plot outline in polar coordinates r(theta)
         plot_angles(rim_list, rim_intp_all[:,it,:], t0)
+        plot_cp_outline(rim_intp, t0)
         plot_cp_outline_alltimes(rim_intp_all[:,0:it+1,:], timerange)
 
 
         ''' Compute radial velocity of rim '''
         rim_vel[0:3, it, :] = rim_intp_all[0:3, it, :]  # copy phi [deg + rad], r(phi)
+
+
         if it == 0:
             rim_vel_av[0, it] = np.average(np.ma.masked_less(rim_intp_all[2, it, :], 1.))
             rim_vel_av[1, it] = 0.0
@@ -298,11 +307,11 @@ def main():
             # for n, phi in enumerate(rim_intp_all[0,it,:]):
             rim_vel[3, it, :] = (rim_intp_all[2, it, :] - rim_intp_all[2, it-1, :]) / dt
             rim_vel_av[0, it] = np.average(np.ma.masked_less(rim_intp_all[2,it,:],1.))
-            # rim_vel_av[0, it] = np.average(np.ma.masked_greater(rim_intp_all[2,it,:],1.))
             rim_vel_av[1, it] = np.average(np.ma.masked_where(rim_intp_all[2,it,:]>1., rim_vel[3,it,:]).data)
 
-            plot_cp_rim_velocity(rim_vel[:, 0:it+1, :], rim_vel_av, timerange)
             plot_cp_rim_averages(rim_vel[:, 0:it+1, :], rim_vel_av[:, :it+1], timerange[:it+1])
+
+        plot_cp_rim_velocity(rim_vel[:, 0:it + 1, :], rim_vel_av, timerange)
 
     return
 
@@ -333,23 +342,22 @@ def plot_cp_rim_averages(rim_vel, rim_vel_av, timerange):
 def plot_cp_rim_velocity(rim_vel, rim_vel_av, timerange):
     nt = rim_vel.shape[1]
     plt.figure(figsize=(12,5))
-    ax = plt.subplot(122)
-    for it, t0 in enumerate(timerange[0:nt]):
-        ax.plot(rim_vel[0, it, :], rim_vel[2, it, :],
-                label='t=' + str(t0) + 's', color=cm_vir(np.double(it) / nt))
-    # plt.legend()
-    # plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.0), ncol=3, fontsize=12)
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=10)
-                   # fancybox=True, shadow=True, ncol=5)
-    plt.xlabel('phi  [deg]')
-    plt.ylabel('U(phi)  [m/s]')
-    plt.ylim([20,np.amax(rim_vel[2,:nt,:])+2])
-
     ax = plt.subplot(121, projection='polar')
     for it, t0 in enumerate(timerange[0:nt]):
-        ax.plot(rim_vel[1, it, :], rim_vel[2, it, :],
+        ax.plot(rim_vel[1, it, :], rim_vel[3, it, :],
                 label='t=' + str(t0) + 's', color=cm_vir(np.double(it) / nt))
-    ax.set_rmax(45.0)
+    # ax.set_rmax(45.0)
+
+    ax = plt.subplot(122)
+    for it, t0 in enumerate(timerange[0:nt]):
+        ax.plot(rim_vel[0, it, :], rim_vel[3, it, :],
+                label='t=' + str(t0) + 's', color=cm_vir(np.double(it) / nt))
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=10)
+    # fancybox=True, shadow=True, ncol=5)
+    plt.xlabel('phi  [deg]')
+    plt.ylabel('U(phi)  [m/s]')
+    # plt.ylim([20,np.amax(rim_vel[2,:nt,:])+2])
+
     plt.suptitle('radial velocity of CP expansion (dr/dt)')
     plt.savefig(os.path.join(path_out, 'rim_velocity.png'))
     plt.close()
@@ -369,6 +377,19 @@ def plot_cp_outline_alltimes(rim_intp_all, timerange):
     plt.savefig(os.path.join(path_out, 'rim_cp1_alltimes.png'))
     plt.close()
 
+
+
+    return
+
+
+
+def plot_cp_outline(rim_intp, t0):
+    plt.figure()
+    ax = plt.subplot(111, projection='polar')
+    ax.plot(rim_intp[1,0:],rim_intp[2,0:],'-o')
+    plt.suptitle('outline CP - t=' + str(t0) + 's', fontsize=28)
+    plt.savefig(os.path.join(path_out, 'rim_cp1_t'+str(t0)+'s.png'))
+    plt.close()
     return
 
 
@@ -583,6 +604,54 @@ def plot_yz_crosssection(w,ic,path_out,t0):
     plt.savefig(os.path.join(path_out, 'w_crosssection_t' + str(t0) + '.png'))
     plt.close()
 
+    return
+
+
+def test_polar_fct():
+    # test polar coordinate transformation
+    xarr = np.arange(-30, 30, 3)
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 3, 1)
+    plt.plot(1, 0, 'o', label='10')
+    plt.plot(0, 1, 'o', label='01')
+    plt.plot(-1, 0, 'o', label='-10')
+    plt.plot(0, -1, 'o', label='0-1')
+    a = 0.707
+    plt.plot(a, a, 'o', label='al=45 d')
+    plt.plot(-a, a, 'o', label='al=45 d')
+    plt.plot(a, -a, 'o', label='al=45 d')
+    plt.plot(-a, -a, 'o', label='al=45 d')
+    plt.legend()
+
+    ax2 = plt.subplot(1, 3, 2)
+    r, th = polar(1, 0)
+    plt.plot(th, r, 'o')
+    r, th = polar(0, 1)
+    plt.plot(th, r, 'o')
+    r, th = polar(-1, 0)
+    plt.plot(th, r, 'o')
+    r, th = polar(0, -1)
+    plt.plot(th, r, 'o')
+
+    r, th = polar(a, a)
+    plt.plot(th, r, 'o')
+    r, th = polar(a, -a)
+    plt.plot(th, r, 'o')
+    r, th = polar(-a, a)
+    plt.plot(th, r, 'o')
+    r, th = polar(-a, -a)
+    plt.plot(th, r, 'o')
+
+    ax2.set_xticks(np.arange(-90, 271, 90), minor=True)
+    ax2.grid(which='minor', color='k', linewidth=0.2)  # , linestyle='-', linewidth=2)
+
+    plt.subplot(1, 3, 3)
+    for i in xarr:
+        for j in xarr:
+            r, th = polar(i, j)
+            plt.plot(th, r, 'o')
+    plt.savefig('./angles_test.png')
+    plt.close()
     return
 
 
