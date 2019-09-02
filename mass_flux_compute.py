c88a73e856ca2b8ee6d2a44c183cc7e26a76ad24
meyerbe <meyerbe@ethz.ch>
2019-09-02 14:23:25 +0200

    plotting time series

--- mass_flux_compute.py
+++ mass_flux_compute.py
@@ -7,6 +7,7 @@ import sys
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 # import matplotlib.cm as cm
+import matplotlib.patches as patch
 import time
 
 
@@ -33,11 +34,12 @@ def main():
 
 
     path, nml = set_input_output_parameters(args)
-    ic_arr, jc_arr = define_geometry(nml)
+    ic_arr, jc_arr, ic, jc = define_geometry(nml)
     path_out_figs = os.path.join(path, 'figs_massflux')
     if not os.path.exists(path_out_figs):
         os.mkdir(path_out_figs)
 
+    global tmin, tmax
     if args.tmin:
         tmin = np.int(args.tmin)
     else:
@@ -111,27 +113,54 @@ def main():
     # compute mass flux
     w = field_k0.variables['w'][:,:,:]
     mf_w = np.sum(w[:itmax+1,:,:], axis=0)
-    # mf_ref = rho0_stats[k0]*np.sum(field_k0.variables['w'], axis=0)
-    aux = np.zeros((w.shape), dtype=np.double)
+    flux = np.zeros((w.shape), dtype=np.double)
     for it in range(itmin, itmax+1):
-        aux[it,:,:] = rho[k0]*w[it,:,:]
-    mf_mean = np.sum(aux, axis=0)
+        flux[it,:,:] = rho[k0]*w[it,:,:]
+    mf_mean = np.sum(flux, axis=0)
     # only account for positive (i.e., upward) mass flux
     mf_w_pos = np.sum(np.where(w[:itmax + 1, :, :] > 0, w[:itmax + 1, :, :], 0), axis=0)
-    mf_mean_pos = np.sum(np.where(w>0, aux, 0), axis=0)
-    del aux
+    flux_pos = np.where(w>0, flux, 0)
+    mf_mean_pos = np.sum(np.where(w>0, flux, 0), axis=0)
+    # del flux
     field_k0.close()
 
 
-
-
+    ''' compute mean flux in box - single, double, triple '''
+    dx_box = 20.
+    dy_box = dx_box
+    d = jc_arr[1] - jc_arr[0]
+    x_single = ic_arr[0] - (rstar + itmax*100) / dx[0]
+    y_single = jc_arr[0] - (rstar + itmax*100) / dx[1]
+    x_double = ic_arr[0] - dx_box / 2
+    y_double = 0.5 * (jc_arr[0] + jc_arr[1]) - dy_box / 2
+    x_triple = ic - dx_box / 2
+    y_triple = jc - dy_box / 2
+    mf_mean_single = np.mean(np.mean(flux[:,x_single:x_single+dx_box, y_single:y_single+dy_box], axis=1), axis=1)
+    mf_mean_double = np.mean(np.mean(flux[:,x_double:x_double+dx_box, y_double:y_double+dy_box], axis=1), axis=1)
+    mf_mean_triple = np.mean(np.mean(flux[:,x_triple:x_triple+dx_box, y_triple:y_triple+dy_box], axis=1), axis=1)
+    mf_mean_single_pos = np.mean(np.mean(flux_pos[:,x_single:x_single+dx_box, y_single:y_single+dy_box], axis=1), axis=1)
+    mf_mean_double_pos = np.mean(np.mean(flux_pos[:,x_double:x_double+dx_box, y_double:y_double+dy_box], axis=1), axis=1)
+    mf_mean_triple_pos = np.mean(np.mean(flux_pos[:,x_triple:x_triple+dx_box, y_triple:y_triple+dy_box], axis=1), axis=1)
+    # accumulated flux
+    mf_mean_single_acc = [np.sum(mf_mean_single[:it+1]) for it in range(itmin,itmax+1)]
+    mf_mean_double_acc = [np.sum(mf_mean_double[:it+1]) for it in range(itmin,itmax+1)]
+    mf_mean_triple_acc = [np.sum(mf_mean_triple[:it+1]) for it in range(itmin,itmax+1)]
+    mf_mean_single_pos_acc = [np.sum(mf_mean_single_pos[:it+1]) for it in range(itmin,itmax+1)]
+    mf_mean_double_pos_acc = [np.sum(mf_mean_double_pos[:it+1]) for it in range(itmin,itmax+1)]
+    mf_mean_triple_pos_acc = [np.sum(mf_mean_triple_pos[:it+1]) for it in range(itmin,itmax+1)]
 
     # ''' plotting '''
     # fig_name = 'massflux_k' + str(k0) + '_tmax' + str(tmax) + '.png'
     # plot_massflux_allversions(mf_mean, mf_mean_pos, mf_w, mf_w_pos, k0, path_out_figs, fig_name)
     fig_name = 'massflux_locations_k' + str(k0) + '_tmax' + str(tmax) + '.png'
-    plot_massflux_locations(mf_mean, mf_mean_pos, mf_w, mf_w_pos,
-                            ic_arr, jc_arr, k0, path_out_figs, fig_name)
+    plot_massflux_locations(mf_mean, mf_mean_pos,
+                            mf_mean_single, mf_mean_double, mf_mean_triple,
+                            mf_mean_single_pos, mf_mean_double_pos, mf_mean_triple_pos,
+                            mf_mean_single_acc, mf_mean_double_acc, mf_mean_triple_acc,
+                            mf_mean_single_pos_acc, mf_mean_double_pos_acc, mf_mean_triple_pos_acc,
+                            ic_arr, jc_arr, ic, jc,
+                            x_single, y_single, x_double, y_double, x_triple, y_triple, dx_box, dy_box,
+                            k0, t_merged, path_out_figs, fig_name)
 
 
     return
@@ -182,36 +211,121 @@ def plot_massflux_allversions(mf_mean, mf_mean_pos, mf_w, mf_w_pos, k0, path_out
 
 
 
-def plot_massflux_locations(mf_mean, mf_mean_pos, mf_w, mf_w_pos,
-                            ic_arr, jc_arr, k0, path_out_figs, fig_name):
-    dx_box = 10
-    dy_box = 10
-    x_single = ic_arr[0]
+def plot_massflux_locations(mf_mean, mf_mean_pos,
+                            mf_mean_single, mf_mean_double, mf_mean_triple,
+                            mf_mean_single_pos, mf_mean_double_pos, mf_mean_triple_pos,
+                            mf_mean_single_acc, mf_mean_double_acc, mf_mean_triple_acc,
+                            mf_mean_single_pos_acc, mf_mean_double_pos_acc, mf_mean_triple_pos_acc,
+                            ic_arr, jc_arr, ic, jc,
+                            x_single, y_single, x_double, y_double, x_triple, y_triple, dx_box, dy_box,
+                            k0, time_range, path_out_figs, fig_name):
 
-    ncol = 2
-    fig, axis = plt.subplots(2, ncol, figsize=(ncol * 5, 10), sharey='all')
+
+    mini = np.maximum(-20, np.int(np.amin(mf_mean)))
+    maxi = np.maximum(np.amax(mf_mean), np.amax(mf_mean_pos))
+    lvls = np.arange(mini, maxi+1, dtype=np.int)
+    # print('---', mini, maxi, lvls[0], lvls[-1])
+    del mini, maxi
+
+    ncol = 4
+    fig, axis = plt.subplots(2, ncol, figsize=(ncol * 5, 10), sharex='none')
     ax = axis[0, 0]
-    cf = ax.contourf(mf_mean.T)
-    for i in range(3):
-        ax.plot(ic_arr[i], jc_arr[i], 'ko', markersize=10)
+    cf = ax.contourf(mf_mean.T, levels=lvls, extend='min')
     plt.colorbar(cf, ax=ax)
     ax.set_title('(A) sum(w[z]*rho[z]) (z=' + str(k0 * dx[2]) + ')')
-    ax = axis[0, 1]
-    cf = ax.contourf(mf_mean.T)
-    plt.colorbar(cf, ax=ax)
-    ax.set_title('(A) sum(w[z]*rho[z]) (z=' + str(k0 * dx[2]) + ')')
-
-
     ax = axis[1, 0]
-    cf = ax.contourf(mf_mean_pos.T)
+    cf = ax.contourf(mf_mean_pos.T, levels=lvls, extend='min')
     plt.colorbar(cf, ax=ax)
     ax.set_title('(B) sum(w[z]*rho[z], w>0) (z=' + str(k0 * dx[2]) + ')')
 
-    # for ax in axis:
-    #     ax.set_aspect('equal')
+    rect_single = patch.Rectangle((x_single,y_single), dx_box, dy_box, fill=False, linewidth=2)
+    rect_double = patch.Rectangle((x_double, y_double), dx_box, dy_box, fill=False, linewidth=2)
+    rect_triple = patch.Rectangle((x_triple, y_triple), dx_box, dy_box, fill=False, linewidth=2)
+    ax = axis[0, 0]
+    ax.add_artist(rect_single)
+    ax.add_artist(rect_double)
+    ax.add_artist(rect_triple)
+    rect_single = patch.Rectangle((x_single, y_single), dx_box, dy_box, fill=False, linewidth=2)
+    rect_double = patch.Rectangle((x_double, y_double), dx_box, dy_box, fill=False, linewidth=2)
+    rect_triple = patch.Rectangle((x_triple, y_triple), dx_box, dy_box, fill=False, linewidth=2)
+    ax = axis[1, 0]
+    ax.add_artist(rect_single)
+    ax.add_artist(rect_double)
+    ax.add_artist(rect_triple)
+    for i in range(2):
+        axis[i, 0].plot(ic_arr[:2], jc_arr[:2], 'ko', markersize=7)
+
+    ax = axis[0, 1]
+    ax.plot(time_range, mf_mean_single, label='flux')
+    ax.plot(time_range, mf_mean_single_pos, '--', label='flux w>0')
+    ax = axis[0, 2]
+    ax.plot(time_range, mf_mean_double, label='flux')
+    ax.plot(time_range, mf_mean_double_pos, '--', label='flux w>0')
+    ax = axis[0, 3]
+    ax.plot(time_range, mf_mean_triple, label='flux')
+    ax.plot(time_range, mf_mean_triple_pos, '--', label='flux w>0')
+
+    ax = axis[1, 1]
+    ax.plot(np.arange(tmin, tmax+dt_fields, dt_fields), mf_mean_single_acc, label='accumulated flux')
+    ax.plot(np.arange(tmin, tmax+dt_fields, dt_fields), mf_mean_single_pos_acc, '--', label='accumulated positive flux')
+    ax = axis[1, 2]
+    ax.plot(np.arange(tmin, tmax+dt_fields, dt_fields), mf_mean_double_acc, label='accumulated flux')
+    ax.plot(np.arange(tmin, tmax+dt_fields, dt_fields), mf_mean_double_pos_acc, '--', label='accumulated positive flux')
+    ax = axis[1, 3]
+    ax.plot(np.arange(tmin, tmax+dt_fields, dt_fields), mf_mean_triple_acc, label='accumulated flux')
+    ax.plot(np.arange(tmin, tmax+dt_fields, dt_fields), mf_mean_triple_pos_acc, '--', label='accumulated positive flux')
+
+    axis[0,1].set_title('flux single')
+    axis[0,2].set_title('flux double')
+    axis[0,3].set_title('flux triple')
+    axis[1,1].set_title('accumulated flux single')
+    axis[1,2].set_title('accumulated flux double')
+    axis[1,3].set_title('accumulated flux triple')
+    for i in range(1,ncol):
+        axis[0,i].set_xlim(tmin,tmax)
+        axis[1,i].set_xlim(tmin,tmax)
+        axis[1,i].set_xlabel('time [s]')
+    delta = (10.*rstar)/dx[0]
+    for i in range(2):
+        axis[i,0].set_aspect('equal')
+        axis[i,0].set_xlim(ic-delta, ic+delta)
+        axis[i,0].set_ylim(jc-delta, jc+delta)
+        axis[i,ncol-1].legend(loc='best')
+
+    plt.suptitle('mass flux (t='+str(tmin)+',..'+str(tmax)+', z='+str(k0*dx[2])+'m)', fontsize=21)
     plt.subplots_adjust(bottom=0.12, right=.95, left=0.07, top=0.9, hspace=0.2, wspace=0.1)
     fig.savefig(os.path.join(path_out_figs, fig_name))
     plt.close(fig)
+
+
+
+
+    # ncol=3
+    # fig, axis = plt.subplots(2, ncol, figsize=(ncol * 5, 10), sharex='none')
+    # ax = axis[0, 0]
+    # cf = ax.contourf(mf_mean.T, levels=lvls)  # , extend='max')
+    # plt.colorbar(cf, ax=ax)
+    # ax = axis[0, 1]
+    # cf = ax.contourf(mf_mean.T, vmin=mini, vmax=maxi)
+    # plt.colorbar(cf, ax=ax)
+    # ax = axis[0, 2]
+    # cf = ax.contourf(mf_mean.T, levels=lvls, extend='min')
+    # plt.colorbar(cf, ax=ax)
+    # ax.set_title('(A) sum(w[z]*rho[z]) (z=' + str(k0 * dx[2]) + ')')
+    # ax = axis[1, 0]
+    # cf = ax.contourf(mf_mean_pos.T, levels=lvls)
+    # plt.colorbar(cf, ax=ax)
+    # ax = axis[1, 1]
+    # cf = ax.contourf(mf_mean_pos.T, vmin=mini, vmax=maxi)
+    # plt.colorbar(cf, ax=ax)
+    # ax = axis[1, 2]
+    # cf = ax.contourf(mf_mean_pos.T, levels=lvls, extend='min')
+    # plt.colorbar(cf, ax=ax)
+    # ax.set_title('(B) sum(w[z]*rho[z], w>0) (z=' + str(k0 * dx[2]) + ')')
+    # plt.suptitle('mass flux (t=' + str(tmin) + ',..' + str(tmax) + ', z=' + str(k0 * dx[2]) + 'm)', fontsize=21)
+    # plt.subplots_adjust(bottom=0.12, right=.95, left=0.07, top=0.9, hspace=0.2, wspace=0.1)
+    # fig.savefig(os.path.join(path_out_figs, 'test_'+fig_name))
+    # plt.close(fig)
 
     return
 # ----------------------------------------------------------------------
@@ -242,11 +356,14 @@ def set_input_output_parameters(args):
 # _______________________________________________________
 
 def define_geometry(nml):
+    global rstar
 
     '''--- define geometry ---'''
-    global rstar
     if case_name == 'ColdPoolDry_double_2D':
-        rstar = 5000.0  # half of the width of initial cold-pools [m]
+        try:
+            rstar = nml['init']['r']
+        except:
+            rstar = 5000.0  # half of the width of initial cold-pools [m]
         irstar = np.int(np.round(rstar / dx))
         isep = 4 * irstar
         ic1 = np.int(nx / 3)
@@ -313,7 +430,7 @@ def define_geometry(nml):
 
 
 
-    return ic_arr, jc_arr
+    return ic_arr, jc_arr, ic, jc
 
 # _______________________________________________________
 
