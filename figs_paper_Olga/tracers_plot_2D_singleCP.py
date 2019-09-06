b8655c065aed3660fd8da20d8181495a897395d4
meyerbe <meyerbe@ethz.ch>
2019-09-06 18:50:39 +0200

    plotting 2D

--- figs_paper_Olga/tracers_plot_2D_singleCP.py
+++ figs_paper_Olga/tracers_plot_2D_singleCP.py
@@ -18,153 +18,226 @@ def main():
     # parser.add_argument("casename")
     # parser.add_argument("path")
     # parser.add_argument("--k0")
-    # parser.add_argument("--tmin")
-    # parser.add_argument("--tmax")
-    # parser.add_argument("--path_tracers")
+    parser.add_argument("--tmin")
+    parser.add_argument("--tmax")
+    parser.add_argument("--dx")
     # parser.add_argument("--shift")
     args = parser.parse_args()
     # set_input_parameters(args)
 
-    # path_olga = '/nbi/ac/conv1/henneb/results/coldpool/lindp2K_13/output/cp/'
-    # path_out_figs = os.path.join('/nbi/ac/conv1/meyerbe/paper_olga/figs_2D/')
-    path_fields = '/Users/bettinameyer/Dropbox/ClimatePhysics/Copenhagen/Projects/ColdPool_LES/data_olga'
-    path_out_figs = os.path.join(path_olga, 'figs_2D')
-    file_olga = 'coldpool_tracer_out_all.txt'
-    CP_id_olga = 3
-    col_id = 4  # column in textfile of CP-ID
+    k0 = 0
+    if args.dx:
+        dx = np.int(args.dx)
+    else:
+        dx = 25
+    # dx = 50
 
+    case_name = 'ColdPoolDry_single_3D'
+    if dx == 25:
+        path_data = '/nbi/ac/cond1/meyerbe/ColdPools/3D_sfc_fluxes_off/single_3D_noise/run4_dx25m/dTh3_z1000_r1000'
+        path_tracer_file = os.path.join(path_data, 'tracer_k' + str(k0) + '/output/', 'coldpool_tracer_out.txt')
+    elif dx == 50:
+        path_data = '/nbi/ac/cond1/meyerbe/ColdPools/3D_sfc_fluxes_off/single_3D_noise/run3_dx50m/dTh3_z1000_r1000'
+        path_tracer_file = os.path.join(path_data, 'tracer_k' + str(k0) + '_nointerpol/output/', 'coldpool_tracer_out.txt')
+    path_fields = os.path.join(path_data, 'fields')
+    path_fields_merged = os.path.join(path_data, 'fields_merged')
+    # path_out_figs = '/Users/bettinameyer/Dropbox/ClimatePhysics/Copenhagen/Projects/ColdPool_LES/data_olga/figs_2D_1CP'
+    path_out_figs = os.path.join('/nbi/ac/conv1/meyerbe/paper_olga/figs_2D_1CP_dx'+str(dx)+'m/')
     if not os.path.exists(path_out_figs):
         os.mkdir(path_out_figs)
 
-    path_tracer_file = os.path.join(path_olga, file_olga)
 
-    global cm_bwr, cm_grey, cm_vir, cm_hsv
-    cm_bwr = plt.cm.get_cmap('bwr')
-    cm_grey = plt.cm.get_cmap('gist_gray_r')
-    cm_hsv = plt.cm.get_cmap('hsv')
-
-    print('???? dt_fields')
-    dt_fields = 100
-    #
-    # if args.k0:
-    #     k0 = np.int(args.k0)
-    # else:
-    #     k0 = 0
-    #
-    # if args.shift:
-    #     shift = np.int(args.shift)
-    # else:
-    #     shift = -1
-    #
-    # if args.path_tracers:
-    #     path_tracers = os.path.join(path, args.path_tracers)
-    #     path_tracer_file = os.path.join(path, args.path_tracers, 'output')
-    # else:
-    #     k_tracers = 0
-    #     path_tracers = os.path.join(path, 'tracer_k' + str(k_tracers))
-    #     path_tracer_file = os.path.join(path, 'tracer_k' + str(k_tracers), 'output')
-    #
-    print('')
-    print('path figs:    ' + path_out_figs)
+    print('path figures: ' + path_out_figs)
     print('path tracers: ' + path_tracer_file)
     print('')
 
-    cp_id = CP_id_olga
-    print('CP ID: ' + str(cp_id))
+    global cm_bwr, cm_grey, cm_vir, cm_hsv
+    cm_bwr = plt.cm.get_cmap('bwr')
+    cm_grey = plt.cm.get_cmap('gray')
+    cm_grey_r = plt.cm.get_cmap('gist_gray_r')
+    cm_hsv = plt.cm.get_cmap('hsv')
+    cm_winter = plt.cm.get_cmap('winter')
+    cm_summer = plt.cm.get_cmap('summer')
+    # cm_fall = plt.cm.get_cmap('fall')
+
+    if args.tmin:
+        tmin = np.int(args.tmin)
+    else:
+        tmin = 0
+    if args.tmax:
+        tmax = np.int(args.tmax)
+    else:
+        tmax = 2500
+    times = np.arange(tmin, tmax, 100)
+    print('tmin: ', tmin)
+    print('tmax: ', tmax)
+    print('times: ' + str(times))
+    nml = simplejson.loads(open(os.path.join(path_data, case_name + '.in')).read())
+    global dt_fields
+    dt_fields = nml['fields_io']['frequency']
+    nx = nml['grid']['nx']
+
+    ''' get tracer coordinates '''
+    cp_id = 1
     n_cps = get_number_cps(path_tracer_file)
-    print('number of CPs: ' + str(n_cps))
     n_tracers = get_number_tracers(path_tracer_file)
+    print('CP ID: ' + str(cp_id))
+    print('number of CPs: ' + str(n_cps))
     print('number of tracers: ' + str(n_tracers))
-    get_tracer_coords(cp_id, n_cps, n_tracers, times, dt_fields, path_tracer_file)
-    # coordinates = get_tracer_coords(cp_id, n_cps, n_tracers, times, dt_fields, path_tracer_file)
     print('')
-    #
-    # '''' plot input fields '''
-    # plot_input_fields(coordinates, n_tracers, shift, path_tracers, path_out_figs)
-    #
-    #
-    # var_list = ['s', 'w']
-    # for it,t0 in enumerate(times):
-    #     print('-plot time: '+str(t0))
-    #     fig_name = 's_w' + '_t' + str(t0) + '_tracers.png'
-    #     fig, axis = plt.subplots(1, 2, figsize=(11, 6), sharey='all')
-    #     rootgrp = nc.Dataset(os.path.join(path_fields, str(t0)+'.nc'))
-    #     for j, var_name in enumerate(var_list):
-    #         print var_name, j
-    #         var = rootgrp.groups['fields'].variables[var_name][:,:,k0]
-    #         max = np.amax(var)
-    #         if var_name in ['w', 'v_rad', 'v_tan']:
-    #             min = -max
-    #             cm_ = cm_bwr
-    #         else:
-    #             min = np.amin(var)
-    #             cm_ = cm_hsv
-    #         # axis[j].contourf(var.T, levels=np.linspace(min, max, 1e2), cmap=cm_)
-    #         axis[j].imshow(var.T, vmin=min, vmax=max, cmap=cm_, origin='lower')
-    #         axis[j].set_title(var_name)
-    #         axis[j].set_aspect('equal')
-    #     rootgrp.close()
+    coordinates = get_tracer_coords(cp_id, n_cps, n_tracers, times, dt_fields, path_tracer_file)
+
+    ''' get 2D field '''
+    var_name = 'w'
+    root = nc.Dataset(os.path.join(path_fields_merged, 'fields_allt_xy_k'+str(k0)+'.nc'))
+    var = root.variables[var_name][:,:,:]
+    root.close()
+    root = nc.Dataset(os.path.join(path_data, 'fields_v_rad', 'v_rad.nc'))
+    v_rad = root.variables['v_rad'][:, :, :, k0]
+    root.close()
+
+    # parameters plotting
+    itmax = np.int(tmax/dt_fields)
+    print('itmax: ', itmax)
+    max_var = np.amax(var[:itmax,:,:])
+    if var_name == 'w':
+        min_var = -max_var
+    else:
+        min_var = np.amin(var[:itmax,:,:])
+    lvls_var = np.linspace(min_var, max_var, 25)
+    max = np.amax(np.abs(v_rad[:,:,:]))
+    lvls_v_rad = np.linspace(-max, max, 25)
+    colmap = cm_hsv
+    colmap = cm_bwr
+    print('min max: ', min_var, max_var)
+
+
+    if dx == 25:
+        imin = 100
+    elif dx == 50:
+        imin = 0
+    imax = nx-imin
+
+
+
+
+
+
+    shift = 0
+    timerange = [0, 900, 1800]
+    fig_name = 'timerange_t' + str(timerange) + '_k' + str(k0) + '.png'
+    fig, axes = plt.subplots(nrows=2, ncols=len(timerange), figsize=(12,7), sharey='row', sharex='col')
+    # for ax in axes.flat:
+    for i,t0 in enumerate(timerange):
+        it = np.int(t0 / dt_fields)
+        ax = axes[0,i]
+        im0 = ax.contourf(var[it, :, :].T, cmap=colmap, levels=lvls_var)
+        ax.set_aspect('equal')
+        ax = axes[1,i]
+        im1 = ax.contourf(v_rad[it, :, :].T, cmap=colmap, levels=lvls_v_rad)
+        ax.set_aspect('equal')
+        for ax in axes[:,i].flat:
+            for i in range(n_tracers):
+                ax.plot(coordinates[it + 1, i, 0] + shift, coordinates[it + 1, i, 1] + shift, 'ok', markersize=1)
+    for ax in axes.flat:
+        ax.set_xlim(imin,imax)
+        ax.set_ylim(imin,imax)
+    for ax in axes[:,0].flat:
+        y_ticks = [np.int((ti-imin) * dx * 1e-3) for ti in ax.get_yticks()]
+        ax.set_yticklabels(y_ticks)
+        ax.set_ylabel('Distance / km')
+    for ax in axes[1,:].flat:
+        x_ticks = [np.int((ti-imin) * dx * 1e-3) for ti in ax.get_xticks()]
+        ax.set_xticklabels(x_ticks)
+        ax.set_xlabel('Distance / km')
+    fig.subplots_adjust(right=0.8)
+    cbar_ax0 = fig.add_axes([0.9, 0.58, 0.015, 0.3])
+    cbar_ax1 = fig.add_axes([0.9, 0.14, 0.015, 0.3])
+    cb0 = fig.colorbar(im0, cax=cbar_ax0, ticks=np.arange(np.floor(min_var), np.floor(max_var) + 1, 1))
+    cb1 = fig.colorbar(im1, cax=cbar_ax1, ticks=[-10,-5,0,5,10])
+    # cb0.set_label('vertical velocity', rotation=270)
+    # cb1.set_label('radial velocity')
+    cb0.set_label('vertical velocity / ms' + r'$^{-1}$')
+    cb1.set_label('radial velocity / ms'+r'$^{-1}$')
+    fig.subplots_adjust(top=0.95, bottom=0.07, left=0.07, right=0.87, hspace=0.1, wspace=0.1)
+    plt.savefig(os.path.join(path_out_figs, fig_name))
+
+
+    colmap = cm_grey_r
+    shift = 0
+    timerange = [0, 900, 1800]
+    fig_name = 'timerange2_t' + str(timerange) + '_k' + str(k0) + '.png'
+    fig, axes = plt.subplots(nrows=2, ncols=len(timerange), figsize=(12, 7), sharey='row', sharex='col')
+    # for ax in axes.flat:
+    for i, t0 in enumerate(timerange):
+        it = np.int(t0 / dt_fields)
+        ax = axes[0, i]
+        im0 = ax.contourf(var[it, :, :].T, cmap=colmap, levels=lvls_var)
+        ax.set_aspect('equal')
+        ax = axes[1, i]
+        im1 = ax.contourf(v_rad[it, :, :].T, cmap=colmap, levels=lvls_v_rad)
+        ax.set_aspect('equal')
+        for ax in axes[:, i].flat:
+            for i in range(n_tracers):
+                ax.plot(coordinates[it + 1, i, 0] + shift, coordinates[it + 1, i, 1] + shift, 'o', color='g', markersize=1)
+    for ax in axes.flat:
+        ax.set_xlim(imin, imax)
+        ax.set_ylim(imin, imax)
+    for ax in axes[:,0].flat:
+        y_ticks = [np.int((ti) * dx * 1e-3) for ti in ax.get_yticks()]
+        ax.set_yticklabels(y_ticks)
+        ax.set_ylabel('Distance / km')
+    for ax in axes[1,:].flat:
+        x_ticks = [np.int((ti-imin) * dx * 1e-3) for ti in ax.get_xticks()]
+        ax.set_xticklabels(x_ticks)
+        ax.set_xlabel('Distance / km')
+    fig.subplots_adjust(right=0.8)
+    cbar_ax0 = fig.add_axes([0.9, 0.58, 0.015, 0.3])
+    cbar_ax1 = fig.add_axes([0.9, 0.14, 0.015, 0.3])
+    cb0 = fig.colorbar(im0, cax=cbar_ax0, ticks=np.arange(np.floor(min_var), np.floor(max_var) + 1, 1))
+    cb1 = fig.colorbar(im1, cax=cbar_ax1, ticks=[-10, -5, 0, 5, 10])
+    cb0.set_label('vertical velocity / ms' + r'$\sum_{i=0}^\infty x_i$')
+    # cb0.set_label('vertical velocity / ms' + r'$s^{-1}$')
+    # cb0.set_label('vertical velocity / ms' + r'$^{-1}$')
+    cb1.set_label('radial velocity / ms' + r'$^{-1}$')
+    fig.subplots_adjust(top=0.95, bottom=0.07, left=0.07, right=0.87, hspace=0.1, wspace=0.1)
+    plt.savefig(os.path.join(path_out_figs, fig_name))
+
+
+
+
+
+    # shift = 0
+    # for t0 in times[:-1]:
+    #     it = np.int(t0 / dt_fields)
+    #     print('plot: t='+str(t0) + ', '+str(it))
+    #     fig_name = var_name + '_t' + str(np.int(t0)) + '_k'+str(k0) + '.png'
+    #     fig, (ax0, ax1) = plt.subplots(1,2, figsize=(20,10))
+    #     for ax in [ax0, ax1]:
+    #         cf = ax.contourf(var[it,:,:].T, cmap=colmap, levels=lvls)
+    #         ax.set_aspect('equal')
+    #         plt.colorbar(cf, ax=ax, shrink=0.5)
     #     for i in range(n_tracers):
-    #         for j in range(len(var_list)):
-    #             axis[j].plot(coordinates[it,i,0]+shift, coordinates[it,i,1]+shift, 'ok', markersize=2)
+    #         ax1.plot(coordinates[it+1, i, 0] + shift, coordinates[it+1, i, 1] + shift, 'ok', markersize=2)
     #     plt.tight_layout()
-    #     fig.savefig(os.path.join(path_out_figs, fig_name))
+    #     plt.savefig(os.path.join(path_out_figs, fig_name))
     #     plt.close(fig)
-    #
-    #
-    # var_list = ['v_rad', 'v_tan']
-    # rootgrp = nc.Dataset(os.path.join(path, 'fields_v_rad', 'v_rad.nc'))
-    # for it, t0 in enumerate(times):
-    #     print('-plot time: ' + str(t0))
-    #     fig_name = 'v_rad_tan' + '_t' + str(t0) + '_tracers.png'
-    #     fig, axis = plt.subplots(1, 2, figsize=(11, 6), sharey='all')
-    #     for j, var_name in enumerate(var_list):
-    #         print var_name, j
-    #         var = rootgrp.variables[var_name][it, :, :, k0]
-    #         max = np.amax(var)
-    #         if var_name in ['w', 'v_rad', 'v_tan']:
-    #             min = -max
-    #         else:
-    #             min = np.amin(var)
-    #         axis[j].contourf(var.T, levels=np.linspace(min, max, 1e2), cmap=cm_bwr)
-    #         axis[j].contourf(var.T, levels=np.linspace(min, max, 1e2), cmap=cm_bwr)
-    #         axis[j].set_title(var_name)
-    #         axis[j].set_aspect('equal')
+
+    # colmap = cm_bwr
+    # for t0 in times:
+    #     it = np.int(t0 / dt_fields)
+    #     print('t: '+str(t0) + ', '+str(it))
+    #     fig_name = 'v_rad' + '_t' + str(np.int(t0)) + '_k'+str(k0) + '.png'
+    #     fig, (ax0, ax1) = plt.subplots(1,2, figsize=(20,10))
+    #     for ax in [ax0, ax1]:
+    #         cf = ax.contourf(v_rad[it,:,:].T, cmap=colmap, levels=lvls)
+    #         ax.set_aspect('equal')
+    #         plt.colorbar(cf, ax=ax, shrink=0.5)
     #     for i in range(n_tracers):
-    #         for j in range(len(var_list)):
-    #             axis[j].plot(coordinates[it,i,0]+shift, coordinates[it,i,1]+shift, 'ok', markersize=2)
+    #         ax1.plot(coordinates[it+1, i, 0] + shift, coordinates[it+1, i, 1] + shift, 'ok', markersize=2)
     #     plt.tight_layout()
-    #     fig.savefig(os.path.join(path_out_figs, fig_name))
+    #     plt.savefig(os.path.join(path_out_figs, fig_name))
     #     plt.close(fig)
-    # rootgrp.close()
-    #
-    #
-    #
-    # var_list = ['u', 'v']
-    # rootgrp = nc.Dataset(os.path.join(path_tracers, 'input', 'uv_alltimes.nc'))
-    # for it, t0 in enumerate(times):
-    #     print('-plot time: ' + str(t0))
-    #     fig_name = 'uv_alltimes' + '_t' + str(t0) + '_tracers.png'
-    #     fig, axis = plt.subplots(1, 2, figsize=(11, 6), sharey='all')
-    #     for j, var_name in enumerate(var_list):
-    #         print var_name, j
-    #         var = rootgrp.variables[var_name][it, :, :]
-    #         max = np.amax(var)
-    #         if var_name in ['w', 'v_rad', 'v_tan']:
-    #             min = -max
-    #         else:
-    #             min = np.amin(var)
-    #         axis[j].contourf(var.T, levels=np.linspace(min, max, 1e2), cmap=cm_bwr)
-    #         axis[j].contourf(var.T, levels=np.linspace(min, max, 1e2), cmap=cm_bwr)
-    #         axis[j].set_title(var_name)
-    #         axis[j].set_aspect('equal')
-    #     for i in range(n_tracers):
-    #         for j in range(len(var_list)):
-    #             axis[j].plot(coordinates[it,i,0]+shift, coordinates[it,i,1]+shift, 'ok', markersize=2)
-    #     plt.tight_layout()
-    #     fig.savefig(os.path.join(path_out_figs, fig_name))
-    #     plt.close(fig)
-    # rootgrp.close()
 
 
     return
@@ -202,52 +275,44 @@ def plot_input_fields(coordinates, n_tracers, shift, path_tracers, path_out_figs
 # ----------------------------------------------------------------------
 
 
-# def get_tracer_coords(cp_id, n_cps, n_tracers, times, dt_fields, fullpath_in):
-def get_tracer_coords(cp_id, n_cps, n_tracers, fullpath_in):
+def get_tracer_coords(cp_id, n_cps, n_tracers, times, dt_fields, fullpath_in):
     print('get tracer coordinates')
     f = open(fullpath_in, 'r')
     lines = f.readlines()
     column = lines[0].split()
 
-    # nt = len(times)
-    # coords = np.zeros((nt, n_tracers, 2))
-    #
-    # for it, t0 in enumerate(times):
-    #     print('----t0='+str(t0), it, '----')
-    #     i = 0
-    #     # count = t0 * n_cps * n_tracers + (cp_id - 1) * n_tracers
-    #     # count = it * n_cps * n_tracers + (cp_id - 1) * n_tracers
-    #     count = t0/dt_fields * n_cps * n_tracers + (cp_id - 1) * n_tracers
-    #     # while CP age is 0 and CP ID is cp_id
-    #     timestep = int(lines[count].split()[0])
-    #     cp_ID = int(lines[count].split()[3])
-    #     # while (timestep - 1 == it and cp_ID == cp_id):
-    #     while (timestep - 1 == t0/dt_fields and cp_ID == cp_id):
-    #         columns = lines[count].split()
-    #         coords[it,i,0] = float(columns[4])
-    #         coords[it,i,1] = float(columns[5])
-    #         i += 1
-    #         count += 1
-    #         cp_ID = int(lines[count].split()[3])
-    #         timestep = int(lines[count].split()[0])
+    nt = len(times)
+    coords = np.zeros((nt, n_tracers, 2))
+
+    for it, t0 in enumerate(times):
+        print('----t0='+str(t0), it, '----')
+        i = 0
+        # count = t0 * n_cps * n_tracers + (cp_id - 1) * n_tracers
+        # count = it * n_cps * n_tracers + (cp_id - 1) * n_tracers
+        count = np.int(t0/dt_fields) * n_cps * n_tracers + (cp_id - 1) * n_tracers
+        # while CP age is 0 and CP ID is cp_id
+        timestep = int(lines[count].split()[0])
+        cp_ID = int(lines[count].split()[3])
+        # while (timestep - 1 == it and cp_ID == cp_id):
+        while (timestep - 1 == t0/dt_fields and cp_ID == cp_id):
+            columns = lines[count].split()
+            coords[it,i,0] = float(columns[4])
+            coords[it,i,1] = float(columns[5])
+            i += 1
+            count += 1
+            cp_ID = int(lines[count].split()[3])
+            timestep = int(lines[count].split()[0])
 
     f.close()
     # print ''
-    # return coords
-    return
+    return coords
 
 
 def get_number_cps(fullpath_in):
     # get number of tracers in each CP
     f = open(fullpath_in, 'r')
     lines = f.readlines()
-    # count = 0
-    # # while CP age is 0 and CP ID is 1
-    # while (int(lines[count].split()[0]) == 1):
-    #     count += 1
-    # cp_number = int(lines[count-1].split()[3])
     cp_number = int(lines[-1].split()[3])
-
     f.close()
 
     return cp_number
