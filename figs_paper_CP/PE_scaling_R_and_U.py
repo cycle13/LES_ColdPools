35ad80094c429be4489e7c7450e5f4145d84c868
Bettina Meyer <bettina.meyer@nbi.ku.dk>
2020-02-21 16:01:05 +0100

    test figure

--- figs_paper_CP/PE_scaling_R_and_U.py
+++ figs_paper_CP/PE_scaling_R_and_U.py
@@ -190,7 +190,7 @@ def test_R_figure(r_av, tmin_r, r_params, dt_fields, path_out_figs, fig_name):
     tmin = tmin_r
 
     R0 = r_av[0, tmin]
-    # dR_log = np.log(r_av[:, tmin]) - np.log(R0)
+    dR_log = np.log(r_av[:, tmin]) - np.log(R0)
     dR = R0 / r_av[:, tmin]
     r_av_ens_mean = np.average(r_av[:, :], axis=0)
     # dR_ens_mean = R0 / r_av_ens_mean[tmin]
@@ -285,7 +285,7 @@ def test_R_figure(r_av, tmin_r, r_params, dt_fields, path_out_figs, fig_name):
     # ax3.set_xlim(np.log(times[tmin]), np.log(times[-2]))
     ax2.set_xlim(0, 2.3)
     ax3.set_xlim(0, 2.3)
-    ax0.set_ylim(0, 8e3)
+    ax0.set_ylim(0, 3)
 
     [ax0, ax1, ax2, ax3] = [axes[4, i] for i in range(4)]
     m = 0.55
@@ -294,31 +294,31 @@ def test_R_figure(r_av, tmin_r, r_params, dt_fields, path_out_figs, fig_name):
         ax0.plot([0, 0], [0, np.amax(r_av)], 'k', linewidth=1)
         ax0.plot(times[4:-1] / t0, r_av[istar, 4:-1] * dR[istar], 'o-',
                  color=colorlist5[istar], label=lbl)
-    #     ax1.loglog(times[4:-1] / t0, r_av[istar, 4:-1] * dR[istar], 'o-', color=colorlist5[istar], label=lbl)
-    #     ax2.plot(np.log(times[4:-1]) - np.log(t0), np.log(r_av[istar, 4:-1]) - dR_log[istar], 'o-',
-    #              color=colorlist5[istar], label=lbl)
-    #     ax3.plot(np.log(times[4:-1]) - np.log(t0), m * (np.log(r_av[istar, 4:-1]) - dR_log[istar]), 'o-',
-    #              color=colorlist5[istar], label=lbl)
-    #     ax2.plot(np.log(times[4:-1]) - np.log(t0),
-    #              np.log(R0) + m * (np.log(times[4:-1]) - np.log(t0)), '-r', label='m=0.54')
-    #
-    #     ax0.plot(times[4:tmin] - t0, r_av[istar, 4:tmin] - dR_log[istar], 'ow')
-    #     ax1.loglog(times[4:tmin] - t0, r_av[istar, 4:tmin], 'ow')
-    #     ax2.plot(np.log(times[4:tmin] - t0), np.log(r_av[istar, 4:tmin]) - dR_log[istar],
-    #              'ow')
-    # ax0.plot(times[4:-1] / t0, r_av_ens_mean[4:-1] * dR_ens_mean, 'b')
-    # ax0.set_xlabel('t/t0')
-    # ax1.set_xlabel('t/t0')
-    # ax2.set_xlabel('log(t)-log(t0)')
-    # ax0.set_ylabel('R * R0(t0)/R(t0)')
-    # ax1.set_ylabel('R * R0(t0)/R(t0)')
-    # ax2.set_ylabel(r'log(R)-dlog(R)')
-    # ax3.set_ylabel(r'log(R)-dlog(R)')
-    # # ax3.set_ylabel('log(R)-[log(R(t0)-log(R0(t0)]')
-    # # ax3.set_xlim(np.log(times[tmin]), np.log(times[-2]))
-    # ax2.set_xlim(0, 2.3)
-    # ax3.set_xlim(0, 2.3)
-    # ax0.set_ylim(0, 8e3)
+        ax1.loglog(times[4:-1] / t0, r_av[istar, 4:-1] * dR[istar], 'o-', color=colorlist5[istar], label=lbl)
+        ax2.plot(np.log(times[4:-1]) - np.log(t0), np.log(r_av[istar, 4:-1]) - dR_log[istar], 'o-',
+                 color=colorlist5[istar], label=lbl)
+        ax3.plot(np.log(times[4:-1]) - np.log(t0), m * (np.log(r_av[istar, 4:-1]) - dR_log[istar]), 'o-',
+                 color=colorlist5[istar], label=lbl)
+        ax2.plot(np.log(times[4:-1]) - np.log(t0),
+                 np.log(R0) + m * (np.log(times[4:-1]) - np.log(t0)), '-r', label='m=0.54')
+
+        ax0.plot(times[4:tmin] - t0, r_av[istar, 4:tmin] - dR_log[istar], 'ow')
+        ax1.loglog(times[4:tmin] - t0, r_av[istar, 4:tmin], 'ow')
+        ax2.plot(np.log(times[4:tmin] - t0), np.log(r_av[istar, 4:tmin]) - dR_log[istar],
+                 'ow')
+    ax0.plot(times[4:-1] / t0, r_av_ens_mean[4:-1] * dR_ens_mean, 'b')
+    ax0.set_xlabel('t/t0')
+    ax1.set_xlabel('t/t0')
+    ax2.set_xlabel('log(t)-log(t0)')
+    ax0.set_ylabel('R * R0(t0)/R(t0)')
+    ax1.set_ylabel('R * R0(t0)/R(t0)')
+    ax2.set_ylabel(r'log(R)-log(R0(t0)/R(t0))')
+    ax3.set_ylabel(r'log(R)-log(R0(t0)/R(t0))')
+    # ax3.set_ylabel('log(R)-[log(R(t0)-log(R0(t0)]')
+    # ax3.set_xlim(np.log(times[tmin]), np.log(times[-2]))
+    ax2.set_xlim(0, 2.3)
+    ax3.set_xlim(0, 2.3)
+    ax0.set_ylim(0, 8e3)
     #
     # [ax0, ax1, ax2, ax3] = [axes[5, i] for i in range(4)]
     # fit_r = np.log(R0) + m * (np.log(times / t0))
