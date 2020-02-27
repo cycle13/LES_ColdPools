1182d64956d3a279fed1846e84a50c42a7e7d6a6
Bettina Meyer <bettina.meyer@nbi.ku.dk>
2020-02-27 21:55:31 +0100

    continuing

--- figs_paper_CP/mass_flux_plot_r2000.py
+++ figs_paper_CP/mass_flux_plot_r2000.py
@@ -121,6 +121,7 @@ def main():
         ''' averaged mass flux '''
         tmin = t_ini[rst][i]
         tmax = t_final[rst][i]
+        delta = 5       # averaging over band of withd delta_y=2*delta*dy=600m
         print('Mass flux: averaging from tmin='+str(tmin)+' to tmax='+str(tmax))
         it0 = np.int(tmin / dt_fields_1CP)
         it1 = np.int(tmax / dt_fields_1CP)
@@ -129,15 +130,105 @@ def main():
         it0 = np.int(tmin / dt_fields_2CP)
         it1 = np.int(tmax / dt_fields_2CP)
         print('MF times 2CP: ', it0, it1)
-        MF_1CP = np.sum(mass_flux_2CP[it0:it1, :, :], axis=0)  # accumulate over time
+        MF_2CP = np.sum(mass_flux_2CP[it0:it1, :, :], axis=0)  # accumulate over time
         it0 = np.int(tmin / dt_fields_3CP)
         it1 = np.int(tmax / dt_fields_3CP)
         print('MF times 3CP: ', it0, it1)
-        MF_1CP = np.sum(mass_flux_3CP[it0:it1, :, :], axis=0)  # accumulate over time
+        MF_3CP = np.sum(mass_flux_3CP[it0:it1, :, :], axis=0)  # accumulate over time
+
+        fig_name = 'collisions_massflux_CPheight_' + case_xCP + '_123.png'
+        plot_collision_massflux_CPheight(MF_1CP, MF_2CP, MF_3CP, #MF_mean_1CP, MF_mean_2CP, MF_mean_3CP,
+                                         t_ini[rst][i], t_2CP[rst][i], t_3CP[rst][i], t_final[rst][i],
+                                         delta, ic_1CP, jc_1CP, ic_2CP, jc_2CP, ic_3CP, jc_3CP,
+                                         ic_arr_2CP, jc_arr_2CP, ic_arr_3CP, jc_arr_3CP,
+                                         xmin_3CP[i], xmax_3CP[i], ymin_3CP[i],
+                                         times, path_out_figs, fig_name)
+
     return
 
 
 
+
+# _______________________________________________________
+def plot_collision_massflux_CPheight(#CP_height_1CP, CP_height_2CP, CP_height_3CP,
+                                     #time_CPheight_1CP, time_CPheight_2CP, time_CPheight_3CP,
+                                     MF_1CP, MF_2CP, MF_3CP, #MF_mean_1CP, MF_mean_2CP, MF_mean_3CP,
+                                     t_ini, t_2CP, t_3CP, t_final,
+                                     delta, ic_1CP, jc_1CP, ic_2CP, jc_2CP, ic_3CP, jc_3CP,
+                                     ic_arr_2CP, jc_arr_2CP, ic_arr_3CP, jc_arr_3CP,
+                                     xmin, xmax, ymin,
+                                     times, path_out_figs, fig_name):
+
+    ncol = 5
+    vmin = 1e-2
+    vmax = 6.5
+    fig, axis = plt.subplots(1, ncol, figsize=(ncol * 5, 5))
+
+    ''' Mass Flux '''
+    ax = axis[1]
+    # ax.plot(np.arange(nx_1CP[1]) - jc_1CP, MF_mean_1CP, label='single CP', color=cm_gray(.1))  # color=colorlist2[0])
+    # ax.plot(np.arange(nx_2CP[1]) - jc_2CP, MF_mean_2CP, label='double collision', color=cm_bwr_r(.8))  # color=colorlist2[0])
+    # ax.plot(np.arange(nx_3CP[0]) - ic_arr_3CP[0], MF_mean_3CP, label='triple collision', color=cm_bwr(.8))  # color=colorlist2[1])
+    # ax.legend(loc=4)
+    # ax.set_xlim(xmin - ic_3CP, xmax - ic_3CP)
+    # ax.set_ylim(-2, np.ceil(np.amax(MF_mean_3CP)))
+    # axis[1].set_ylabel(r'Integrated Mass Flux  [kg/m$^2$]')
+
+
+    ''' Mass Flux 2D '''
+    ax = axis[2]
+    b = ic_arr_3CP[2]-ic_arr_3CP[0]
+    pcm = ax.pcolormesh(np.arange(nx_1CP[0])-ic_1CP+b, np.arange(nx_1CP[1])-jc_1CP, MF_1CP,
+                        norm=colors.LogNorm(vmin=vmin, vmax=vmax), cmap=cm_bw)  # cmap='RdBu_r')
+    rect1 = mpatches.Rectangle((-ic_1CP, -delta), nx_1CP[0], 2*delta, fill=True,
+                               linewidth=0, edgecolor='r', facecolor='lightyellow', alpha=0.3)
+    ax.add_patch(rect1)
+    # ax.set_xlim(xmin-ic_3CP, ic_3CP-xmin)
+    # ax.set_ylim(ymin-ic_3CP, ic_3CP-ymin)
+    ax = axis[3]
+    pcm = ax.pcolormesh(np.arange(nx_2CP[1])-jc_2CP, np.arange(nx_2CP[0])-ic_2CP, MF_2CP,
+                        # norm = colors.SymLogNorm(linthresh=0.03,linscale=0.03,vmin=-max,vmax=max),
+                        # norm = colors.PowerNorm(),
+                        norm=colors.LogNorm(vmin=vmin, vmax=vmax), cmap=cm_bw)  # cmap='RdBu_r')
+    rect = mpatches.Rectangle((-jc_2CP, -delta), nx_2CP[1], 2*delta, fill=True,
+                              linewidth=0, edgecolor='r', facecolor='lightyellow', alpha=0.3)
+    ax.add_patch(rect)
+    ax.set_xlim(xmin-ic_3CP, ic_3CP-xmin)
+    ax.set_ylim(ymin-ic_3CP, ic_3CP-ymin)
+    ax = axis[4]
+    pcm = ax.pcolormesh(np.arange(nx_3CP[0])-ic_arr_3CP[0], np.arange(nx_3CP[1])-jc_3CP, MF_3CP.T,
+                        norm=colors.LogNorm(vmin=vmin, vmax=vmax), cmap=cm_bw)  # cmap='RdBu_r')
+    plt.colorbar(pcm, ax=ax, extend='both')
+    # rect2 = mpatches.Rectangle((-100, jc_arr_3CP[2] - delta), 300, 2*delta, fill=True,
+    rect2 = mpatches.Rectangle((-100, -delta), 300, 2*delta, fill=True,
+                               linewidth=0, edgecolor='r', facecolor='lightyellow', alpha=0.3)
+    ax.add_patch(rect2)
+    ax.set_xlim(xmin-ic_3CP, xmax-ic_3CP)
+    ax.set_ylim(ymin-ic_3CP, ic_3CP-ymin)
+    for ax in axis[2:].flat:
+        ax.set_aspect('equal')
+    axis[2].set_ylabel('y [km]')
+
+    for ax in axis:
+        ax.set_xlabel('x [km]')
+        ax.set_xticklabels([np.int(ti*1e-3*dx[0]) for ti in ax.get_xticks()])
+    axis[0].set_yticklabels([np.round(ti*1e-3,1) for ti in axis[0].get_yticks()])
+    axis[1].set_yticklabels([np.round(ti,1) for ti in axis[1].get_yticks()])
+    for ax in axis[2:]:
+        ax.set_yticklabels([np.int(ti*1e-3*dx[0]) for ti in ax.get_yticks()])
+
+    # textprops = dict(facecolor='white', alpha=0.9, linewidth=0.)
+    # axis[0].text(xmin-ic_3CP+15, 900, 'a)', fontsize=18, bbox=textprops)
+    # axis[1].text(xmin-ic_3CP+15, np.amax(MF_mean_3CP)-.3, 'b)', fontsize=18, bbox=textprops)
+    # axis[2].text(xmin-ic_3CP+15, ic_3CP-ymin-30, 'c)', fontsize=21, bbox=textprops)
+    # axis[3].text(xmin-ic_3CP+15, ic_3CP-ymin-30, 'd)', fontsize=21, bbox=textprops)
+    # axis[4].text(xmin-ic_3CP+15, ic_3CP-ymin-30, 'e)', fontsize=21, bbox=textprops)
+
+    plt.subplots_adjust(bottom=0.1, right=.99, left=0.03, top=0.95, hspace=0.2, wspace=0.25)
+    fig.savefig(os.path.join(path_out_figs, fig_name))
+    plt.close(fig)
+
+    return
 # _______________________________________________________
 
 def set_input_output_parameters(args, case_name_1CP, case_name_2CP, case_name_3CP,
